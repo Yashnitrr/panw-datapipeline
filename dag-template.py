3e44087a6a5612d147a02b5bda38db3efe9795f0
Yash <yashnitrr@gmail.com>
2019-05-16 18:03:21 +0530

    dag-template updated

--- dag-template.py
+++ dag-template.py
@@ -34,7 +34,7 @@ default_args = {
 }
 
 dag = DAG(
-    'airflow_demo',
+    'airflow_task',
     default_args=default_args,
     description='Orchestration DAG',
     schedule_interval=timedelta(days=1), 
