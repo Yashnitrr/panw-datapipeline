00f53a544832aa6454c37d994181b6007da14b0d
venkypuppala <48930436+venkypuppala@users.noreply.github.com>
2019-06-03 09:04:53 -0700

    Update dag-template.py

--- dag-template.py
+++ dag-template.py
@@ -34,7 +34,7 @@ default_args = {
 }
 
 dag = DAG(
-    'airflow_demo4',
+    'airflow_demo5',
     default_args=default_args,
     description='Orchestration DAG',
     schedule_interval=timedelta(days=1), 
